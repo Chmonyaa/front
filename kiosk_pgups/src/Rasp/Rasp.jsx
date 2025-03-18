96aa9380f15042368d1c83ce06bbdaab88b121ef
Olga Khonina <106494000+Chmonyaa@users.noreply.github.com>
2025-03-18 18:04:51 +0300

    Update Rasp.jsx

--- kiosk_pgups/src/Rasp/Rasp.jsx
+++ kiosk_pgups/src/Rasp/Rasp.jsx
@@ -1,22 +1,100 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Rasp.css'
+import './Rasp.css';
 import RealTimeClock from '../RealTimeClock.jsx';
+
 function Rasp() {
     const navigate = useNavigate();
-    const handleClick1 = () => {
-        navigate('/main');
+
+    const [step, setStep] = useState(1);
+    const [selectedFaculty, setSelectedFaculty] = useState('');
+    const [selectedCourse, setSelectedCourse] = useState('');
+    const [selectedGroup, setSelectedGroup] = useState('');
+    const [selectedSubgroup, setSelectedSubgroup] = useState('');
+    const [faculties, setFaculties] = useState([]);
+    const [courses, setCourses] = useState([]);
+    const [filteredGroups, setFilteredGroups] = useState([]);
+    const [, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://dev-info-board.ru/api/v1/schedule/faculties/')
+            .then(response => response.json())
+            .then(data => {
+                setFaculties(data);
+                setIsLoading(false); // Отключаем загрузку
+            })
+            .catch(error => {
+                console.error('Ошибка загрузки данных факультетов:', error);
+                setIsLoading(false); // Даже в случае ошибки скрываем загрузку
+            });
+    }, []);
+
+    const loadCoursesForFaculty = (facultyId) => {
+        fetch(`https://dev-info-board.ru/api/v1/schedule/faculties-groups/`)
+            .then(response => response.json())
+            .then(data => {
+                const facultyData = data.find(f => f.id === facultyId);
+                if (facultyData && facultyData.students_groups) {
+                    const uniqueCourses = [...new Set(facultyData.students_groups.map(group => group.course_number))]
+                        .sort((a, b) => a - b); // Сортируем по возрастанию
+                    setCourses(uniqueCourses);
+                } else {
+                    setCourses([]);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка загрузки курсов для факультета:', error);
+                setCourses([]);
+            });
     };
-    const handleClick2 = () => {
-        navigate('/map');
+
+    const handleSearch = (facultyId, courseNumber) => {
+        const url = `https://dev-info-board.ru/api/v1/schedule/faculties-groups/${facultyId}/?course=${courseNumber}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.students_groups) {
+                    const sortedGroups = data.students_groups.sort((a, b) => a.name.localeCompare(b.name));
+                    setFilteredGroups(sortedGroups);
+                } else {
+                    setFilteredGroups([]);
+                }
+            })
+            .catch(error => console.error('Ошибка загрузки групп:', error));
     };
-    const handleClick3 = () => {
-        navigate('/info');
+
+    const handleClick1 = () => navigate('/main');
+    const handleClick2 = () => navigate('/map');
+    const handleClick3 = () => navigate('/info');
+    const handleFacultyClick = (faculty) => {
+        setSelectedFaculty(faculty);
+        setStep(2);
+        const facultyId = faculties.find(f => f.short_name === faculty)?.id;
+        loadCoursesForFaculty(facultyId);
     };
+
+    const handleCourseClick = (course) => {
+        setSelectedCourse(course);
+        const facultyId = faculties.find(faculty => faculty.short_name === selectedFaculty)?.id;
+        handleSearch(facultyId, course);
+        setStep(3);
+    };
+
+    const handleGroupClick = (group) => {
+        setSelectedGroup(group);
+        setStep(4);
+    };
+
+    const handleSubgroupClick = (subgroup) => {
+        setSelectedSubgroup(subgroup);
+        setStep(5);
+    };
+
     return (
         <>
             <div className="your_css_selector"></div>
             <div className="frame">
-                <RealTimeClock/>
+                <RealTimeClock />
                 <div className="menu">
                     <button className="menubutton" onClick={handleClick1}>Новости</button>
                     <button className="selectButton">Расписание</button>
@@ -28,12 +106,84 @@ function Rasp() {
                     <button className="toggle-button">文</button>
                 </div>
                 <div className="search-container">
-                    <input type="text" id="search" placeholder="Поиск..."/>
+                    <input type="text" id="search" placeholder="Поиск..." />
                     <div className="divider"></div>
                     <button className="Search" onClick="searchFunction()"></button>
                 </div>
             </div>
 
+            {/* Выбор факультета */}
+            <div className='choice-faculty' style={{ display: step === 1 ? 'block' : 'none' }}>
+                <div className="rasp-choice-container">
+                    <div className="text-container">
+                        {faculties.length > 0 && <h1>Выберите факультет</h1>}
+                    </div>
+                    <div className="rasp-buttons-container">
+                        {faculties.map((faculty, index) => (
+                            <button key={index} className="rasp-choice-button" onClick={() => handleFacultyClick(faculty.short_name)}>
+                                {faculty.short_name}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
+            {/* Выбор курса */}
+            <div className='choice-course' style={{ display: step === 2 ? 'block' : 'none' }}>
+                <div className="rasp-choice-container">
+                    <div className="text-container">
+                        {courses.length > 0 && <h1>Выберите курс</h1>}
+                    </div>
+                    <div className="rasp-buttons-container">
+                        {courses.map((course, index) => (
+                            <button key={index} className="rasp-choice-button" onClick={() => handleCourseClick(course)}>
+                                {course}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
+            {/* Выбор группы */}
+            <div className='choice-group' style={{ display: step === 3 ? 'block' : 'none' }}>
+                <div className="rasp-choice-container">
+                    <div className="text-container">
+                        {filteredGroups.length > 0 && <h1>Выберите группу</h1>}
+                    </div>
+                    <div className="rasp-buttons-container">
+                        {filteredGroups.map((group, index) => (
+                            <button key={index} className="rasp-choice-button" onClick={() => handleGroupClick(group.name)}>
+                                {group.name}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
+            {/* Выбор подгруппы */}
+            <div className='choice-subgroup' style={{ display: step === 4 ? 'block' : 'none' }}>
+                <div className="rasp-choice-container">
+                    <div className="text-container">
+                        <h1>Выберите подгруппу</h1>
+                    </div>
+                    <div className="rasp-buttons-container">
+                        {['1', '2'].map((subgroup, index) => (
+                            <button key={index} className="rasp-choice-button" onClick={() => handleSubgroupClick(subgroup)}>{subgroup}</button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            {/* Расписание */}
+            <div className='rasp' style={{ display: step === 5 ? 'block' : 'none' }}>
+                <p className="test">Факультет {selectedFaculty}</p>   {/*закомментировать потом*/}
+                <p className="test">Группа {selectedGroup}</p>
+                <p className="test">Курс {selectedCourse}</p>
+                <p className="test">Подгруппа {selectedSubgroup}</p>
+            </div>
         </>
     );
 }
